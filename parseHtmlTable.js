c4bdc9cce02021897174cf03f8617211490af63f
Adam <vynpch6qaj@snkmail.com>
2019-05-31 14:54:58 -0700

    Update parseHtmlTable.js

--- parseHtmlTable.js
+++ parseHtmlTable.js
@@ -1,43 +1,63 @@
+// USE AT https://www.w3schools.com/html/html_tables.asp
+
 'use strict';
 
+var table5 = document.querySelectorAll('table')[5].querySelector('tbody');
+
+var tableHeadRow = table5.querySelectorAll('th');
+
+var tableHeadRowData = []
+
+tableHeadRow.forEach((cell) => {tableHeadRowData.push(cell.innerText);});
+
+console.log('--------------------');
+console.log('tableHeadRowData');
+console.log(tableHeadRowData);
+console.log('--------------------');
+
+var tableRows = table5.querySelectorAll('tr');
+
+console.log('tableRows');
+console.log(tableRows);
+console.log('--------------------');
+
+var tableRowData = [];
+
+tableRows.forEach((row) => {
+	if (!!row.querySelector('th')) {return;}
+	var rowData = [];
+	row.querySelectorAll('td').forEach((cell) => {rowData.push(cell.innerText);});
+	tableRowData.push(rowData);
+})
+
+console.log('tableRowData');
+console.log(tableRowData);
+console.log('--------------------');
+
 var headers = {
-	orig: {
-		test:/pickup/i,
+	desc: {
+		test:/description/i,
 		position: undefined
 	},
-	dest: {
-		test:/drop\soff/i,
-		position: undefined
-	},
-	loadId: {
-		test:/load\snumber/i,
+	tag: {
+		test:/tag/i,
 		position: undefined
 	}
 };
 
-// PLACEHOLDER FOR ACTUAL HEADERS FROM PAGE
-var arrayOfThText = ['Pickup', 'Drop Off', 'Load Number'];
-
 // GET POSITION OF THE DESIRED HEADERS
 Object.keys(headers).forEach((keyName) => {
-	var keyPosition = arrayOfThText.findIndex((element) => {
+	var keyPosition = tableHeadRowData.findIndex((element) => {
 		return headers[keyName].test.test(element);
 	});
 	headers[keyName].position = keyPosition;
 })
 
-// PLACEHOLDER FOR TABLE VALUES FROM PAGE
-var arrayOfTrs = [
-	['Phoenix', 'Fontana', '12345'],
-	['Phoenix', 'Fontana', '12346'],
-	['Phoenix', 'Fontana', '12347']
-];
-
 // VAR TO RECEIVE PARSED CONTENTS OF TABLE
 var tableContents = [];
 
 // PARSE EACH ROW
-arrayOfTrs.forEach((htmlTableRow) => {
+tableRowData.forEach((htmlTableRow) => {
 	var parsedTableRow = {};
 	Object.keys(headers).forEach((keyName) => {
 		parsedTableRow[keyName] = htmlTableRow[headers[keyName].position];
